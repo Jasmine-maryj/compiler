f27ceb359b486240637a20116cbc5494ce8fcc5d
Jasmine <95772825+Jasmine-maryj@users.noreply.github.com>
2023-09-27 19:03:31 +0530

    Update ParserRuleContext.java

--- src/com/dev/ParserRuleContext.java
+++ src/com/dev/ParserRuleContext.java
@@ -1,4 +1,46 @@
 package com.dev;
 
-public class ParserRuleContext {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class ParserRuleContext implements ParseTree {
+
+  private ParseTree parent;
+  private List<ParseTree> children;
+
+  @Override
+  public ParseTree getParent() {
+    return parent;
+  }
+
+  @Override
+  public void setParent(ParseTree parent) {
+    this.parent = parent;
+  }
+
+  @Override
+  public Object getPayload() {
+    return this;
+  }
+
+  public void addChild(ParseTree child) {
+    child.setParent(this);
+    if (children == null) {
+      children = new ArrayList<>();
+    }
+    children.add(child);
+  }
+
+  @Override
+  public ParseTree getChild(int i) {
+    return children.get(i);
+  }
+
+  @Override
+  public int getChildCount() {
+    return (children != null) ? children.size() : 0;
+  }
+
+  public abstract <T> T accept(Visitor<? extends T> visitor);
 }
+
