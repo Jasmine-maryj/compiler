7008fde89a550d8d70bfcb1dc694f132b8b648fc
Jasmine <95772825+Jasmine-maryj@users.noreply.github.com>
2023-09-28 22:38:44 +0530

    Update LetContext.java

--- src/com/dev/LetContext.java
+++ src/com/dev/LetContext.java
@@ -1,4 +1,27 @@
 package com.dev;
 
-public class LetContext {
+import org.antlr.v4.runtime.Token;
+
+public class LetContext extends ParserRuleContext {
+    private final Token variableName;
+    private final Token variableValue;
+
+    public LetContext(Token variableName, Token variableValue) {
+        this.variableName = variableName;
+        this.variableValue = variableValue;
+    }
+
+    public Token getVariableName() {
+        return variableName;
+    }
+
+    public Token getVariableValue() {
+        return variableValue;
+    }
+
+    @Override
+    public <T> T accept(Visitor<? extends T> visitor) {
+        return visitor.visitLet(this);
+    }
 }
+
